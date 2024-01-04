a05dbed7e3d7d98b2e61d088e830c35bc07af830
Marek Petrik <mpetrik@cs.unh.edu>
2024-01-04 13:06:15 -0500

    updated erm test case

--- test/src/erm.jl
+++ test/src/erm.jl
@@ -4,12 +4,12 @@ using RiskMDPs
 using RiskMeasures
 using Distributions
 include("make_domains.jl")
-prob = TestDomains["onestatepm"]
+prob = TestDomains["inventory_generic"]
 
 # evaluates the policy by simulation
 function evaluate_sim(prob::Problem, π, β)
     # evaluation helper variables
-    episodes = 100000
+    episodes = 10000000
     # reward weights
     rweights::Vector{Float64} = prob.γ .^ (0:prob.horizon-1)     
     # distribution over episodes
@@ -17,12 +17,16 @@ function evaluate_sim(prob::Problem, π, β)
     
     H = simulate(prob.model, π, prob.initstate, prob.horizon, episodes)
     returns = rweights' * H.rewards |> vec
-    return ERM(returns, ones(length(returns)) / length(returns), β)
+    returns
 end
 
 #@testset "Nested Simple" begin
     for (probname, prob) ∈ TestDomains
         println(probname)
+
+        # change the parameters to make the solutions closer together
+        prob = @set prob.β = 10.0
+        prob = @set prob.horizon = 6
 
         # Discounted ERM
         obj = DiscountedERM(prob.γ, prob.β, prob.horizon)
@@ -30,10 +34,12 @@ end
         v = vp.value
         π = vp.policy
 
-        ret_erm = evaluate_sim(prob, π, prob.β)
+        rets = evaluate_sim(prob, π, prob.β)
+        ret_erm = ERM(rets, ones(length(rets)) / length(rets), prob.β)
         println("Expected ERM return: ", v[1][prob.initstate])
         println("Simulated ERM return: ", ret_erm)
 
+        println(isapprox(v[1][prob.initstate], ret_erm, rtol=0.2, atol=5.0))
         # ret_mean should be computed using the optimal policy
         #@test ret_cvar ≤ ret_mean + 0.1 * abs(ret_mean) + 0.1
         
